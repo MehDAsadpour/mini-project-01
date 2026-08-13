f9edde91bf3175a24eaddf78647c98e3afc9e23b
MehDAsadpour <mehdiasadpour.ai@gmail.com>
2026-08-14 00:06:52 +0330

    feature: use data prep scaler in training pipeline

--- src/train.py
+++ src/train.py
@@ -1,37 +1,106 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from data_prep import prepare_data
-from sklearn.metrics import(accuracy_score,
-                            recall_score,
-                            precision_score,
-                            f1_score,
-                            confusion_matrix)
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import StratifiedKFold, cross_validate
+from sklearn.metrics import (
+    accuracy_score,
+    recall_score,
+    precision_score,
+    f1_score,
+    confusion_matrix
+)
 
-def evaluate_model(model,X_test,y_test):
+from data_prep import prepare_data,get_scaler
+
+def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
-    print("Recall :",recall_score(y_test,y_pred))
-    print("Precision :",precision_score(y_test,y_pred))
-    print("F1 Score :",f1_score(y_test,y_pred))
-    cm = confusion_matrix(y_test,y_pred)
-    print("confusion matrix :\n",cm)
-    print("Accuracy :",accuracy_score(y_test,y_pred))
-    print('_'*100)
 
-X_train,X_test,X_train_scaled,X_test_scaled,y_train,y_test,scaler = prepare_data(
+    print("Recall :", recall_score(y_test, y_pred))
+    print("Precision :", precision_score(y_test, y_pred))
+    print("F1 Score :", f1_score(y_test, y_pred))
+
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix :\n", cm)
+
+    print("Accuracy :", accuracy_score(y_test, y_pred))
+    print("_" * 100)
+
+
+def evaluate_train_model(model, X_train, y_train):
+    y_pred_train = model.predict(X_train)
+
+    print("Train Recall :", recall_score(y_train, y_pred_train))
+    print("Train Precision :", precision_score(y_train, y_pred_train))
+    print("Train F1 Score :", f1_score(y_train, y_pred_train))
+
+
+def cross_validate_model(model, X, y, cv):
+    results = cross_validate(
+        model,
+        X,y,
+        cv=cv,
+        scoring= ["precision","recall","f1"]
+    )
+    mean_precision = results["test_precision"].mean()
+    mean_recall = results["test_recall"].mean()
+    mean_f1 = results["test_f1"].mean()
+
+    print("Mean Precision :", mean_precision)
+    print("Mean Recall :", mean_recall)
+    print("Mean F1 :", mean_f1)
+    print("_" * 100)
+
+
+X_train,X_test,y_train,y_test = prepare_data(
     "data/creditcard.csv"
     )
 
-logistic_model = LogisticRegression(random_state=42)
-logistic_model.fit(X_train_scaled,y_train)
-evaluate_model(logistic_model,X_test_scaled,y_test)
+logistic_pipeline = Pipeline([
+    ("scaler", get_scaler()),
+    ("model", LogisticRegression(random_state=42))
+])
+logistic_pipeline.fit(X_train, y_train)
+evaluate_model(
+    logistic_pipeline,
+    X_test,
+    y_test
+)
 
 # k = 5 (default)
-knn_model = KNeighborsClassifier()
-knn_model.fit(X_train_scaled,y_train)
-evaluate_model(knn_model,X_test_scaled,y_test)
+knn_pipeline = Pipeline([
+    ("scaler", get_scaler()),
+    ("model", KNeighborsClassifier())
+])
+knn_pipeline.fit(X_train, y_train)
+evaluate_model(
+    knn_pipeline,
+    X_test,
+    y_test
+)
 
 # Unlimited-depth baseline Decision Tree (possible overfitting)
-decision_tree_model = DecisionTreeClassifier(random_state=42)
-decision_tree_model.fit(X_train,y_train)
-evaluate_model(decision_tree_model,X_test,y_test)
+decision_tree_model = DecisionTreeClassifier(
+    random_state=42
+)
+decision_tree_model.fit(X_train, y_train)
+evaluate_model(
+    decision_tree_model,
+    X_test,
+    y_test
+)
+evaluate_train_model(
+    decision_tree_model,
+    X_train,
+    y_train
+)
+
+cv = StratifiedKFold(
+    n_splits=5,
+    shuffle=True,
+    random_state=42
+)
+
+cross_validate_model(logistic_pipeline,X_train,y_train,cv)
+cross_validate_model(knn_pipeline,X_train,y_train,cv)
+cross_validate_model(decision_tree_model,X_train,y_train,cv)
