eee00a77a1a53484ab700c138a1d829c60827cb4
MehDAsadpour <mehdiasadpour.ai@gmail.com>
2026-08-14 19:17:36 +0330

    feat: add KNN scaling and hyperparameter experiments

--- src/train.py
+++ src/train.py
@@ -13,9 +13,10 @@ from sklearn.metrics import (
 
 from data_prep import prepare_data,get_scaler
 
-def evaluate_model(model, X_test, y_test):
+def evaluate_model(model, X_test, y_test,model_name):
     y_pred = model.predict(X_test)
 
+    print(f"\n==={model_name}===\n")
     print("Recall :", recall_score(y_test, y_pred))
     print("Precision :", precision_score(y_test, y_pred))
     print("F1 Score :", f1_score(y_test, y_pred))
@@ -35,7 +36,7 @@ def evaluate_train_model(model, X_train, y_train):
     print("Train F1 Score :", f1_score(y_train, y_pred_train))
 
 
-def cross_validate_model(model, X, y, cv):
+def cross_validate_model(model, X, y, cv,model_name):
     results = cross_validate(
         model,
         X,y,
@@ -46,6 +47,7 @@ def cross_validate_model(model, X, y, cv):
     mean_recall = results["test_recall"].mean()
     mean_f1 = results["test_f1"].mean()
 
+    print(f"\n==={model_name}===\n")
     print("Mean Precision :", mean_precision)
     print("Mean Recall :", mean_recall)
     print("Mean F1 :", mean_f1)
@@ -56,6 +58,12 @@ X_train,X_test,y_train,y_test = prepare_data(
     "data/creditcard.csv"
     )
 
+
+#==============================================================
+# BaseLine Models
+#==============================================================
+
+# Logistic Regression
 logistic_pipeline = Pipeline([
     ("scaler", get_scaler()),
     ("model", LogisticRegression(random_state=42))
@@ -64,10 +72,11 @@ logistic_pipeline.fit(X_train, y_train)
 evaluate_model(
     logistic_pipeline,
     X_test,
-    y_test
+    y_test,
+    "Logistic Regression - Baseline"
 )
 
-# k = 5 (default)
+# k = 5 (default) with Scaling
 knn_pipeline = Pipeline([
     ("scaler", get_scaler()),
     ("model", KNeighborsClassifier())
@@ -76,7 +85,8 @@ knn_pipeline.fit(X_train, y_train)
 evaluate_model(
     knn_pipeline,
     X_test,
-    y_test
+    y_test,
+    "KNN - Baseline (k=5, with Scaling)"
 )
 
 # Unlimited-depth baseline Decision Tree (possible overfitting)
@@ -87,13 +97,14 @@ decision_tree_model.fit(X_train, y_train)
 evaluate_model(
     decision_tree_model,
     X_test,
-    y_test
+    y_test,
+    "Decision Tree - Baseline (max_depth=None)"
 )
-evaluate_train_model(
-    decision_tree_model,
-    X_train,
-    y_train
-)
+
+
+#==============================================================
+# Cross Validation BaseLine Models
+#==============================================================
 
 cv = StratifiedKFold(
     n_splits=5,
@@ -101,6 +112,51 @@ cv = StratifiedKFold(
     random_state=42
 )
 
-cross_validate_model(logistic_pipeline,X_train,y_train,cv)
-cross_validate_model(knn_pipeline,X_train,y_train,cv)
-cross_validate_model(decision_tree_model,X_train,y_train,cv)
+cross_validate_model(
+    logistic_pipeline,
+    X_train,y_train,
+    cv,
+    "Cross Validation - Logistic Regression"
+)
+
+cross_validate_model(
+    knn_pipeline,
+    X_train,
+    y_train,
+    cv,
+    "Cross Validation - KNN (k=5)"
+)
+
+cross_validate_model(
+    decision_tree_model,
+    X_train,
+    y_train,
+    cv,
+    "Cross Validation - Decision Tree"
+)
+
+
+#==============================================================
+# Experiment 1: Effect of Scaling
+#==============================================================
+
+# KNN without Scaling
+knn_no_scale = KNeighborsClassifier()
+knn_no_scale.fit(X_train,y_train)
+evaluate_model(
+    knn_no_scale,
+    X_test,
+    y_test,
+    "Experiment 1 - KNN without Scaling"
+)
+
+#==============================================================
+# Experiment 2: KNN Hyperparameter Analysis
+#==============================================================
+
+for k in [1, 5, 20]:
+    knn_pipeline.set_params(model__n_neighbors=k)
+
+    knn_pipeline.fit(X_train, y_train)
+
+    evaluate_model(knn_pipeline, X_test, y_test,f"KNN Hyperparameter Analysis : k = {k}")
